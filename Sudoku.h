b0abe31047ca65fac2afe7342a2ecf2fe12fef19
ICHIA830506 <yohot506@gmail.com>
2016-04-01 15:05:33 +0800

    almost finished_revised

--- Sudoku.h
+++ Sudoku.h
@@ -12,31 +12,28 @@ class Sudoku
 	
 	Sudoku();
 	Sudoku(vector<int>,int);
+	
     void giveQuestion();//fun.1
 	void readIn();//fun.2
 	void solve();//fun.3
-	
-	vector<int> getBoard();//
-	void setAns(int A);//
-	int getAns();//
-	
-	//
 	void change();
-	void changeNum(int a,int b);
-	void changeRow(int a,int b);
-	void changeCol(int a,int b);
-	void rotate(int n);
-	void flip(int n);
-	//
+	void changeNum(int a,int b);//fun.4
+	void changeRow(int a,int b);//fun.5
+	void changeCol(int a,int b);//fun.6
+	void rotate(int n);//fun.7
+	void flip(int n);//fun.8
+	void transform();//fun.9
+	
+	vector<int> getBoard();
+	void setAns(int A);
+	int getAns();
 	
 	static const int sudokuSize = 81;
 	static const int Size = 9;
 	
 	private:
 	
-	
 	int map[sudokuSize];
-	
 	int count[sudokuSize];
 	int Found;	
 	bool tag[sudokuSize][Size];
